4354d24d1c64473390beb391d6d6a953921ede3c
cadorn <christoph@christophdorn.com>
2013-01-14 13:00:20 -0800

    catch error

--- bin/sm-cli.js
+++ bin/sm-cli.js
@@ -2,8 +2,12 @@
 const SM_CLI = require("../lib/sm-cli");
 const ERROR = require("sourcemint-util-js/lib/error");
 
-SM_CLI.for(process.cwd()).respond(process.argv).then(function() {
-	process.exit(0);
-}, function(err) {
+try {
+	SM_CLI.for(process.cwd()).respond(process.argv).then(function() {
+		process.exit(0);
+	}, function(err) {
+		return ERROR.exitProcessWithError(err);
+	});
+} catch(err) {
 	return ERROR.exitProcessWithError(err);
-});
+}
